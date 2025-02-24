6668eac00407a93f9ba613d08cd3222af7b732d6
Jang-Junha618 <167044369+Jang-Junha618@users.noreply.github.com>
2025-02-24 18:37:21 +0900

    [add] 채팅 구현(서버와 클라이언트 연결)

---
+++ ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Chat_Program
+{
+    public class ChatHub
+    {
+        public static ChatHub Parse(string json)
+        {
+            var chatHub = JsonSerializer.Deserialize<ChatHub>(json);
+
+            // null 체크
+            if (chatHub == null)
+            {
+                throw new InvalidOperationException("Deserialization failed: chatHub is null.");
+            }
+
+            return chatHub;
+        }
+
+
+        public string user_id { get; set; } = string.Empty;
+        public string message { get; set; } = string.Empty;
+
+        public string ToJsonString => JsonSerializer.Serialize(this);
+    }
+}
